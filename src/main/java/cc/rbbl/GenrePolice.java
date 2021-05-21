2530b2bb30b8f05b9e88070a9f16d71cf88b50f0
rbbl-dev <dev@rbbl.cc>
2021-05-21 14:35:46 +0200

    added shutdown event listeners to shutdown container when the bot is disconnected from discord

--- src/main/java/cc/rbbl/GenrePolice.java
+++ src/main/java/cc/rbbl/GenrePolice.java
@@ -5,16 +5,28 @@ import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.DisconnectEvent;
+import net.dv8tion.jda.api.events.ReconnectedEvent;
+import net.dv8tion.jda.api.events.ResumedEvent;
+import net.dv8tion.jda.api.events.ShutdownEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.apache.hc.core5.http.ParseException;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
 import java.io.IOException;
+import java.net.UnknownHostException;
 
-public class GenrePolice extends ListenerAdapter {
+public class GenrePolice extends ListenerAdapter implements Runnable {
 
+    private static final long RECONNECTION_TIMEOUT = 70000L;
+
+    private boolean isConnected = true;
+    private static final Logger logger = LoggerFactory.getLogger(GenrePolice.class);
     private final SpotifyLinkHandler spotifyLinkHandler;
 
     public static void main(String[] args) throws LoginException, IllegalArgumentException, ParseException, SpotifyWebApiException, IOException {
@@ -22,6 +34,7 @@ public class GenrePolice extends ListenerAdapter {
 
         // We only need 2 intents in this bot. We only respond to messages in guilds and private channels.
         // All other events will be disabled.
+
         JDABuilder.createLight(params.getDiscordToken(), GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
                 .addEventListeners(new GenrePolice(params))
                 .setActivity(Activity.watching("Spotify Links"))
@@ -33,6 +46,36 @@ public class GenrePolice extends ListenerAdapter {
     }
 
     @Override
+    public void onDisconnect(@NotNull DisconnectEvent event) {
+        super.onDisconnect(event);
+        isConnected = false;
+        Thread thread = new Thread(this);
+        logger.error("Disconnected. Starting reconnect timeout of " + RECONNECTION_TIMEOUT + " mills");
+        thread.start();
+    }
+
+    @Override
+    public void onReconnected(@NotNull ReconnectedEvent event) {
+        super.onReconnected(event);
+        isConnected = true;
+        logger.info("Reconnected!");
+    }
+
+    @Override
+    public void onResumed(@NotNull ResumedEvent event) {
+        super.onResumed(event);
+        isConnected = true;
+        logger.info("Reconnected!");
+    }
+
+    @Override
+    public void onShutdown(@NotNull ShutdownEvent event) {
+        super.onShutdown(event);
+        logger.error("Discord Shutdown Event occurred. Shutting down Process.", event);
+        System.exit(1);
+    }
+
+    @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         Message msg = event.getMessage();
         if (SpotifyLinkHandler.isApplicableMsg(msg.getContentRaw())) {
@@ -41,7 +84,7 @@ public class GenrePolice extends ListenerAdapter {
                 response = genresToMessage(spotifyLinkHandler.getGenres(msg.getContentRaw()));
             } catch (NoGenreFoundException e) {
                 response = "Spotify has no genre for that Item";
-            }finally {
+            } finally {
                 msg.reply(response).queue();
             }
         }
@@ -55,7 +98,19 @@ public class GenrePolice extends ListenerAdapter {
         return message;
     }
 
-    private boolean emptyOrNull(String[] input) {
-        return input == null || input.length == 0;
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(RECONNECTION_TIMEOUT);
+            if (!isConnected) {
+                logger.error("Shutting down after waiting " + RECONNECTION_TIMEOUT + " mills to reconnect.");
+                System.exit(1);
+            }
+        } catch (InterruptedException e) {
+            if (!isConnected) {
+                logger.error("Reconnection Thread interrupted. Shutting down immediately.", e);
+                System.exit(1);
+            }
+        }
     }
 }
