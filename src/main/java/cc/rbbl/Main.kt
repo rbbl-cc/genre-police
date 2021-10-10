450de799aed7d970ee335dba389ed88bef52b581
rbbl-dev <dev@rbbl.cc>
2021-10-10 19:55:22 +0200

    migrated Main Class to kotlin

--- src/main/java/cc/rbbl/Main.kt
+++ src/main/java/cc/rbbl/Main.kt
@@ -1,73 +1,65 @@
-package cc.rbbl;
+package cc.rbbl
 
-import cc.rbbl.persistence.MessageEntity;
-import cc.rbbl.program_parameters_jvm.ParameterDefinition;
-import cc.rbbl.program_parameters_jvm.ParameterHolder;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.utils.cache.CacheFlag;
-import org.apache.commons.dbcp2.BasicDataSource;
-import org.flywaydb.core.Flyway;
-import org.hibernate.cfg.AvailableSettings;
-import org.hibernate.cfg.Configuration;
+import cc.rbbl.program_parameters_jvm.ParameterDefinition
+import cc.rbbl.program_parameters_jvm.ParameterHolder
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.requests.GatewayIntent
+import net.dv8tion.jda.api.utils.cache.CacheFlag
+import org.apache.commons.dbcp2.BasicDataSource
+import org.flywaydb.core.Flyway
+import org.hibernate.cfg.AvailableSettings
+import javax.persistence.EntityManagerFactory
+import javax.persistence.Persistence
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.security.auth.login.LoginException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+fun main(args: Array<String>) {
+    val params = ParameterHolder(
+        setOf(
+            ParameterDefinition("DISCORD_TOKEN", caseSensitive = false),
+            ParameterDefinition("SPOTIFY_CLIENT_ID", caseSensitive = false),
+            ParameterDefinition("SPOTIFY_CLIENT_SECRET", caseSensitive = false),
+            ParameterDefinition("DB_USER", caseSensitive = false),
+            ParameterDefinition("DB_PASSWORD", caseSensitive = false),
+            ParameterDefinition("JDBC_URL", caseSensitive = false)
+        )
+    )
+    params.loadParametersFromEnvironmentVariables()
+    params.loadParameters(args)
+    params.checkParameterCompleteness()
+    handleDbMigration(params)
 
-public class Main {
-    public static void main(String[] args) throws LoginException, IllegalArgumentException {
-        ParameterHolder params = new ParameterHolder(Set.of(
-                new ParameterDefinition("DISCORD_TOKEN", true, false),
-                new ParameterDefinition("SPOTIFY_CLIENT_ID", true, false),
-                new ParameterDefinition("SPOTIFY_CLIENT_SECRET", true, false),
-                new ParameterDefinition("DB_USER", true, false),
-                new ParameterDefinition("DB_PASSWORD", true, false),
-                new ParameterDefinition("JDBC_URL", true, false)
-        ));
-        params.loadParametersFromEnvironmentVariables();
-        params.loadParameters(args);
-        params.checkParameterCompleteness();
+    // We only need 2 intents in this bot. We only respond to messages in guilds and private channels.
+    // All other events will be disabled.
+    JDABuilder.create(
+        params["DISCORD_TOKEN"], GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES,
+        GatewayIntent.GUILD_MESSAGE_REACTIONS, GatewayIntent.DIRECT_MESSAGE_REACTIONS
+    )
+        .disableCache(
+            CacheFlag.ACTIVITY, CacheFlag.VOICE_STATE, CacheFlag.EMOTE,
+            CacheFlag.CLIENT_STATUS, CacheFlag.ONLINE_STATUS
+        )
+        .addEventListeners(GenrePolice(params, getHibernateConfig(params)))
+        .setActivity(Activity.watching("Spotify Links"))
+        .build()
+}
 
-        handleDbMigration(params);
+private fun handleDbMigration(parameters: ParameterHolder) {
+    val flyway = Flyway.configure().dataSource(
+        parameters["JDBC_URL"],
+        parameters["DB_USER"],
+        parameters["DB_PASSWORD"]
+    ).load()
+    flyway.migrate()
+}
 
-        // We only need 2 intents in this bot. We only respond to messages in guilds and private channels.
-        // All other events will be disabled.
-        JDABuilder.create(params.get("DISCORD_TOKEN"), GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES,
-                        GatewayIntent.GUILD_MESSAGE_REACTIONS, GatewayIntent.DIRECT_MESSAGE_REACTIONS)
-                .disableCache(CacheFlag.ACTIVITY, CacheFlag.VOICE_STATE, CacheFlag.EMOTE,
-                        CacheFlag.CLIENT_STATUS, CacheFlag.ONLINE_STATUS)
-                .addEventListeners(new GenrePolice(params, getHibernateConfig(params)))
-                .setActivity(Activity.watching("Spotify Links"))
-                .build();
-    }
-
-    private static void handleDbMigration(ParameterHolder parameters) {
-        Flyway flyway = Flyway.configure().dataSource(
-                parameters.get("JDBC_URL"),
-                parameters.get("DB_USER"),
-                parameters.get("DB_PASSWORD")
-        ).load();
-        flyway.migrate();
-    }
-
-    private static EntityManagerFactory getHibernateConfig(ParameterHolder parameters) {
-        BasicDataSource dataSource = new BasicDataSource();
-
-        dataSource.setPassword(parameters.get("DB_PASSWORD"));
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(parameters.get("JDBC_URL"));
-        dataSource.setUsername(parameters.get("DB_USER"));
-
-        Map<String, Object> properties = new HashMap<>();
-        properties.put(AvailableSettings.DATASOURCE, dataSource);
-        properties.put(AvailableSettings.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
-        return Persistence.createEntityManagerFactory("p-unit", properties);
-
-
-    }
+private fun getHibernateConfig(parameters: ParameterHolder): EntityManagerFactory {
+    val dataSource = BasicDataSource()
+    dataSource.password = parameters["DB_PASSWORD"]
+    dataSource.driverClassName = "org.postgresql.Driver"
+    dataSource.url = parameters["JDBC_URL"]
+    dataSource.username = parameters["DB_USER"]
+    val properties: MutableMap<String?, Any?> = HashMap()
+    properties[AvailableSettings.DATASOURCE] = dataSource
+    properties[AvailableSettings.DIALECT] = "org.hibernate.dialect.PostgreSQLDialect"
+    return Persistence.createEntityManagerFactory("p-unit", properties)
 }
