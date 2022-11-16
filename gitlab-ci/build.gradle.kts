39b6665e01ae88993a93eff5ff00577f1f822c4a
rbbl-dev <dev@rbbl.cc>
2022-11-16 23:06:56 +0100

    migrated ci to kotlin definition

--- gitlab-ci/build.gradle.kts
+++ gitlab-ci/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.pcimcioch:gitlab-ci-kotlin-dsl:1.3.1")
+    implementation("com.github.pcimcioch:gitlab-ci-kotlin-dsl:1.3.2")
 }
 
 
--- gitlab-ci/src/main/java/cc/rbbl/Main.kt
+++ gitlab-ci/src/main/java/cc/rbbl/Main.kt
@@ -1,20 +1,41 @@
 package cc.rbbl
 
-import pcimcioch.gitlabci.dsl.Duration
 import pcimcioch.gitlabci.dsl.gitlabCi
-import pcimcioch.gitlabci.dsl.job.WhenUploadType
-import java.io.FileWriter
+import pcimcioch.gitlabci.dsl.job.createJob
+import pcimcioch.gitlabci.dsl.job.createRules
 
 object Stages {
-    val Build = "build"
-    val Docker = "docker"
-    val Helm = "helm"
-    val Release = "release"
+    const val Build = "build"
+    const val Docker = "docker"
+    const val Helm = "helm"
+    const val Release = "release"
+}
+
+object Rules {
+    val dev = createRules {
+        rule {
+            ifCondition = "\$CI_COMMIT_BRANCH =~ /^dev$/"
+        }
+    }
+    val master = createRules {
+        rule {
+            ifCondition = "\$CI_COMMIT_BRANCH =~ /^master$/"
+        }
+    }
+    val releaseCandidate = createRules {
+        rule {
+            ifCondition = "\$CI_COMMIT_TAG =~ /^\\d+\\.\\d+\\.\\d+-RC\\d+$/"
+        }
+    }
+    val release = createRules {
+        rule {
+            ifCondition = "\$CI_COMMIT_TAG =~ /^\\d+\\.\\d+\\.\\d+$/"
+        }
+    }
 }
 
 fun main() {
-    val writer = FileWriter("../.gitlab-ci.yml")
-    gitlabCi(validate = true, writer = writer) {
+    gitlabCi(validate = true, "../.gitlab-ci.yml") {
         stages {
             +Stages.Build
             +Stages.Docker
@@ -22,7 +43,7 @@ fun main() {
             +Stages.Release
         }
 
-        job("build") {
+        val buildJob = createJob("build") {
             stage = Stages.Build
             image("gradle:7.2.0-jdk11")
             script("gradle build")
@@ -30,19 +51,117 @@ fun main() {
                 paths("app/build/libs/genre-police.jar")
             }
         }
+        +buildJob
 
-        job("release") {
-            stage = "release"
-            script("./gradlew publishToMavenLocal")
-            only {
-                master()
-            }
-            artifacts {
-                whenUpload = WhenUploadType.ON_SUCCESS
-                paths("build/libs")
-                expireIn = Duration(days = 7)
+
+        val dockerBaseJob = createJob(".docker") {
+            stage = Stages.Docker
+            image("docker")
+            services("docker:dind")
+            beforeScript("docker login -u \$CI_REGISTRY_USER -p \$CI_REGISTRY_PASSWORD \$CI_REGISTRY")
+        }
+        +dockerBaseJob
+
+
+        val dockerBuildJob = createJob("docker-build") {
+            extends(dockerBaseJob)
+            needs(buildJob)
+            script(
+                "docker build -t \$CI_REGISTRY_IMAGE:\$CI_COMMIT_SHORT_SHA ./app",
+                "docker push \$CI_REGISTRY_IMAGE:\$CI_COMMIT_SHORT_SHA"
+            )
+        }
+        +dockerBuildJob
+
+        job("docker-publish-dev") {
+            needs(dockerBuildJob)
+            extends(dockerBaseJob)
+            script(
+                "docker pull \$CI_REGISTRY_IMAGE:\$CI_COMMIT_SHORT_SHA",
+                "docker tag \$CI_REGISTRY_IMAGE:\$CI_COMMIT_SHORT_SHA rbbl/genre-police:dev",
+                "docker login -u \$DOCKERHUB_USER -p \$DOCKERHUB_ACCESS_TOKEN",
+                "docker push rbbl/genre-police:dev"
+            )
+            rules = Rules.dev
+        }
+
+        job("docker-publish-master") {
+            needs(dockerBuildJob)
+            extends(dockerBaseJob)
+            script(
+                "docker pull \$CI_REGISTRY_IMAGE:\$CI_COMMIT_SHORT_SHA",
+                "docker tag \$CI_REGISTRY_IMAGE:\$CI_COMMIT_SHORT_SHA rbbl/genre-police:latest",
+                "docker login -u \$DOCKERHUB_USER -p \$DOCKERHUB_ACCESS_TOKEN",
+                "docker push rbbl/genre-police:latest"
+            )
+            rules = Rules.master
+        }
+
+        job("docker-publish-release") {
+            needs(dockerBuildJob)
+            extends(dockerBaseJob)
+            script(
+                "docker pull \$CI_REGISTRY_IMAGE:\$CI_COMMIT_SHORT_SHA",
+                "docker tag \$CI_REGISTRY_IMAGE:\$CI_COMMIT_SHORT_SHA rbbl/genre-police:\$CI_COMMIT_TAG",
+                "docker login -u \$DOCKERHUB_USER -p \$DOCKERHUB_ACCESS_TOKEN",
+                "docker push rbbl/genre-police:\$CI_COMMIT_TAG"
+            )
+            rules = Rules.release
+        }
+
+        job("docker-publish-release-candidate") {
+            needs(dockerBuildJob)
+            extends(dockerBaseJob)
+            script(
+                "docker pull \$CI_REGISTRY_IMAGE:\$CI_COMMIT_SHORT_SHA",
+                "docker tag \$CI_REGISTRY_IMAGE:\$CI_COMMIT_SHORT_SHA rbbl/genre-police:\$CI_COMMIT_TAG",
+                "docker tag \$CI_REGISTRY_IMAGE:\$CI_COMMIT_SHORT_SHA \$JFROG_URL/genre-police-docker-local/genre-police:\$CI_COMMIT_TAG",
+                "docker login -u \$DOCKERHUB_USER -p \$DOCKERHUB_ACCESS_TOKEN",
+                "docker login -u \$JFROG_USERNAME -p \$JFROG_API_KEY \$JFROG_URL",
+                "docker push rbbl/genre-police:\$CI_COMMIT_TAG",
+                "docker push \$JFROG_URL/genre-police-docker-local/genre-police:\$CI_COMMIT_TAG"
+            )
+            rules = Rules.releaseCandidate
+        }
+
+        val helmBaseJob = createJob(".helm") {
+            stage = Stages.Helm
+            image("fedora")
+            beforeScript(
+                "dnf in -y openssl",
+                "curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3",
+                "chmod 700 get_helm.sh",
+                "./get_helm.sh",
+                "helm repo add bitnami https://charts.bitnami.com/bitnami",
+                "helm dependency build helm/genre-police",
+                "helm package helm/genre-police",
+                "mv genre-police-*.tgz genre-police.tgz"
+            )
+        }
+        +helmBaseJob
+
+        job("helm-publish-release") {
+            extends(helmBaseJob)
+            rules = Rules.release
+            script("curl --request POST --user gitlab-ci-token:\$CI_JOB_TOKEN --form 'chart=@genre-police.tgz' \${CI_API_V4_URL}/projects/\${CI_PROJECT_ID}/packages/helm/api/stable/charts")
+        }
+
+        job("helm-publish-release-candidate") {
+            extends(helmBaseJob)
+            rules = Rules.releaseCandidate
+            script("curl --request POST --user gitlab-ci-token:\$CI_JOB_TOKEN --form 'chart=@genre-police.tgz' \${CI_API_V4_URL}/projects/\${CI_PROJECT_ID}/packages/helm/api/dev/charts")
+        }
+
+        job("create-release") {
+            stage = Stages.Release
+            image("registry.gitlab.com/gitlab-org/release-cli:latest")
+            rules = Rules.release
+            script("echo 'Running the release job.'")
+            release {
+                name = "\$CI_COMMIT_TAG"
+                tagName = "\$CI_COMMIT_TAG"
+                description = "Release \$CI_COMMIT_TAG"
             }
         }
     }
-    writer.close()
 }
