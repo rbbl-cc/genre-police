bf1ee0d80fd2cc1d7788baca491b51ef1eed4734
rbbl-dev <dev@rbbl.cc>
2023-07-29 05:30:59 +0200

    fixed build

--- gitlab-ci/build.gradle.kts
+++ gitlab-ci/build.gradle.kts
@@ -12,12 +12,12 @@ repositories {
         name = "Snapshot"
         url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
     }
+    mavenLocal()
 }
 
 dependencies {
     implementation("com.github.pcimcioch:gitlab-ci-kotlin-dsl:1.6.0")
     implementation("cc.rbbl.gitlab-ci-kotlin-dsl-extensions:common-jobs:1.0.1")
-    implementation("cc.rbbl.gitlab-ci-kotlin-dsl-extensions:container-common:0.1.0-SNAPSHOT")
     implementation("cc.rbbl.gitlab-ci-kotlin-dsl-extensions:podman:0.1.0-SNAPSHOT")
 }
 
