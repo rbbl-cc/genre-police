f3d9f5b38739133adeec2509432fe73281cbd292
rbbl-dev <dev@rbbl.cc>
2023-08-26 23:25:04 +0200

    improved handling of short links

--- app/src/test/kotlin/cc/rbbl/link_handlers/SpotifyMessageHandlerTest.kt
+++ app/src/test/kotlin/cc/rbbl/link_handlers/SpotifyMessageHandlerTest.kt
@@ -10,91 +10,91 @@ internal class SpotifyMessageHandlerTest {
     @Test
     fun `regular track url`() {
         assertTrue {
-            SpotifyMessageHandler.SPOTIFY_LINK_PATTERN.matches("https://open.spotify.com/track/2p4p9YGwmJIdf5IA9sSWhm?si=ac6b3105df714569")
+            SpotifyMessageHandler.SPOTIFY_LONG_LINK_PATTERN.matches("https://open.spotify.com/track/2p4p9YGwmJIdf5IA9sSWhm?si=ac6b3105df714569")
         }
     }
 
     @Test
     fun `regular anonymous track url`() {
         assertTrue {
-            SpotifyMessageHandler.SPOTIFY_LINK_PATTERN.matches("https://open.spotify.com/track/2p4p9YGwmJIdf5IA9sSWhm")
+            SpotifyMessageHandler.SPOTIFY_LONG_LINK_PATTERN.matches("https://open.spotify.com/track/2p4p9YGwmJIdf5IA9sSWhm")
         }
     }
 
     @Test
     fun `regular album url`() {
         assertTrue {
-            SpotifyMessageHandler.SPOTIFY_LINK_PATTERN.matches("https://open.spotify.com/album/5WS1g0cKtjfK6eDoSLdv7d?si=ff8085a125f24d7d")
+            SpotifyMessageHandler.SPOTIFY_LONG_LINK_PATTERN.matches("https://open.spotify.com/album/5WS1g0cKtjfK6eDoSLdv7d?si=ff8085a125f24d7d")
         }
     }
 
     @Test
     fun `regular anonymous album url`() {
         assertTrue {
-            SpotifyMessageHandler.SPOTIFY_LINK_PATTERN.matches("https://open.spotify.com/album/5WS1g0cKtjfK6eDoSLdv7d")
+            SpotifyMessageHandler.SPOTIFY_LONG_LINK_PATTERN.matches("https://open.spotify.com/album/5WS1g0cKtjfK6eDoSLdv7d")
         }
     }
 
     @Test
     fun `regular artist url`() {
         assertTrue {
-            SpotifyMessageHandler.SPOTIFY_LINK_PATTERN.matches("https://open.spotify.com/artist/2o8lOQRjzsSC8UdbNN88HN?si=F970p5vnSpeFWeYz1uFpwQ")
+            SpotifyMessageHandler.SPOTIFY_LONG_LINK_PATTERN.matches("https://open.spotify.com/artist/2o8lOQRjzsSC8UdbNN88HN?si=F970p5vnSpeFWeYz1uFpwQ")
         }
     }
 
     @Test
     fun `regular anonymous artist url`() {
         assertTrue {
-            SpotifyMessageHandler.SPOTIFY_LINK_PATTERN.matches("https://open.spotify.com/artist/2o8lOQRjzsSC8UdbNN88HN")
+            SpotifyMessageHandler.SPOTIFY_LONG_LINK_PATTERN.matches("https://open.spotify.com/artist/2o8lOQRjzsSC8UdbNN88HN")
         }
     }
 
     @Test
     fun `regular episode url`() {
         assertFalse {
-            SpotifyMessageHandler.SPOTIFY_LINK_PATTERN.matches("https://open.spotify.com/episode/0UJirr3XsKjh2VI18aM6Bj?si=f918ba9433f041f6")
+            SpotifyMessageHandler.SPOTIFY_LONG_LINK_PATTERN.matches("https://open.spotify.com/episode/0UJirr3XsKjh2VI18aM6Bj?si=f918ba9433f041f6")
         }
     }
 
     @Test
     fun `regular anonymous episode url`() {
         assertFalse {
-            SpotifyMessageHandler.SPOTIFY_LINK_PATTERN.matches("https://open.spotify.com/episode/0UJirr3XsKjh2VI18aM6Bj")
+            SpotifyMessageHandler.SPOTIFY_LONG_LINK_PATTERN.matches("https://open.spotify.com/episode/0UJirr3XsKjh2VI18aM6Bj")
         }
     }
 
     @Test
     fun `regular show url`() {
         assertFalse {
-            SpotifyMessageHandler.SPOTIFY_LINK_PATTERN.matches("https://open.spotify.com/show/6KnaAHvqf0pgTs3Kw3qQTR?si=cad36accf40245b3")
+            SpotifyMessageHandler.SPOTIFY_LONG_LINK_PATTERN.matches("https://open.spotify.com/show/6KnaAHvqf0pgTs3Kw3qQTR?si=cad36accf40245b3")
         }
     }
 
     @Test
     fun `regular anonymous show url`() {
         assertFalse {
-            SpotifyMessageHandler.SPOTIFY_LINK_PATTERN.matches("https://open.spotify.com/show/6KnaAHvqf0pgTs3Kw3qQTR")
+            SpotifyMessageHandler.SPOTIFY_LONG_LINK_PATTERN.matches("https://open.spotify.com/show/6KnaAHvqf0pgTs3Kw3qQTR")
         }
     }
 
     @Test
     fun `market track url`() {
         assertTrue {
-            SpotifyMessageHandler.SPOTIFY_LINK_PATTERN.matches("https://open.spotify.com/intl-de/track/4awF9g7FMdeTxeD4OaSUIa?si=e6d6d7cc272c4873")
+            SpotifyMessageHandler.SPOTIFY_LONG_LINK_PATTERN.matches("https://open.spotify.com/intl-de/track/4awF9g7FMdeTxeD4OaSUIa?si=e6d6d7cc272c4873")
         }
     }
 
     @Test
     fun `market anonymous track url`() {
         assertTrue {
-            SpotifyMessageHandler.SPOTIFY_LINK_PATTERN.matches("https://open.spotify.com/intl-de/track/4awF9g7FMdeTxeD4OaSUIa")
+            SpotifyMessageHandler.SPOTIFY_LONG_LINK_PATTERN.matches("https://open.spotify.com/intl-de/track/4awF9g7FMdeTxeD4OaSUIa")
         }
     }
 
     @Test
     fun `short track url`() {
         assertTrue {
-            SpotifyMessageHandler.SPOTIFY_LINK_PATTERN.matches("https://spotify.link/elxIFeXj2Ab")
+            SpotifyMessageHandler.SPOTIFY_LONG_LINK_PATTERN.matches("https://spotify.link/elxIFeXj2Ab")
         }
     }
 
